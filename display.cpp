edbf232aeb1f0ee12c086fb65a2d7e06007d48e4
FatcatX <garretsimpson@yahoo.com>
2024-09-26 21:36:37 -0700

    Added display.cpp

---
+++ display.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+
+#include "3ps/ska/bytell_hash_map.hpp"
+#include "shapez.hpp"
+
+#define NUM 10
+
+int main(int argc, char* argv[]) {
+  using namespace Shapez;
+
+  if (argc != 2) {
+    std::cout << "Usage: display dump.bin" << std::endl;
+    return 1;
+  }
+
+  ShapeSet set = ShapeSet::load(argv[1]);
+  // ska::bytell_hash_set<Shape> halves{set.halves.begin(), set.halves.end()};
+
+  std::cout << "Halves: " << set.halves.size() << std::endl;
+  Shape shape;
+  size_t num;
+
+  num = set.halves.size();
+  for (size_t i = 0; i < num; ++i) {
+    shape = set.halves[i];
+    std::cout << shape.toString() << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << "Shapes: " << set.shapes.size() << std::endl;
+  num = set.shapes.size();
+  for (size_t i = 0; i < num; ++i) {
+    shape = set.shapes[i];
+    std::cout << shape.toString() << std::endl;
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
