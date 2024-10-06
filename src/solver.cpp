ad2bba5537c966985386d5f2963f75df03cfb67c
FatcatX <garretsimpson@yahoo.com>
2024-10-05 18:31:29 -0700

    Added verifyShapes
    Checks all non-swap shapes to make sure they are really not made from halves.

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 
+#include "3ps/ska/bytell_hash_map.hpp"
 #include "shapez.hpp"
 
 namespace Shapez {
@@ -11,8 +12,12 @@ struct Solver {
   constexpr static size_t LAYER = Shape::LAYER;
 
   ShapeSet set;
+  ska::bytell_hash_set<Shape> halves;
 
-  Solver(char* filename) { set = ShapeSet::load(filename); }
+  Solver(char* filename) {
+    set = ShapeSet::load(filename);
+    halves = {set.halves.begin(), set.halves.end()};
+  }
 
   // Whether a shape can be constructed by swapping two halves.
   bool swappable(Shape shape) const {
@@ -20,36 +25,45 @@ struct Solver {
     for (size_t angle = 0; angle < PART / 2; ++angle) {
       Shape left{shape.rotate(angle).value & mask};
       Shape right{shape.rotate(angle + PART / 2).value & mask};
-      std::cout << left.toString() << std::endl;
-      std::cout << right.toString() << std::endl;
+      // std::cout << left.toString() << std::endl;
+      // std::cout << right.toString() << std::endl;
+      left = left.equivalentHalves()[0];
+      right = right.equivalentHalves()[0];
       if (left.value == 0 || right.value == 0) return true;
+      if (halves.find(left) != halves.end() &&
+          halves.find(right) != halves.end()) {
+        return true;
+      }
     }
 
     return false;
   }
 
   void verifyShapes() {
-    Shape shape;
     size_t num;
+    Shape shape;
+    size_t found = 0;
+    // shape = {"SSS-:----:----:----"};
 
     num = set.shapes.size();
     std::cout << "Shapes: " << num << std::endl;
-    // for (size_t i = 0; i < num; ++i) {
-    //   shape = set.halves[i];
-    //   std::cout << shape.toString() << std::endl;
-    // }
-    shape = set.shapes[0];
-    std::cout << shape.toString() << std::endl;
-    bool result = swappable(shape);
-    std::cout << "result: " << result << std::endl;
+    for (size_t i = 0; i < num; ++i) {
+      shape = set.shapes[i];
+      if (swappable(shape)) {
+        std::cout << "Found: " << shape.toString() << std::endl;
+        found++;
+      }
+    }
+    if (found == 0)
+      std::cout << "None found" << std::endl;
+    else
+      std::cout << "Found: " << found << std::endl;
   }
 };
 
 }  // namespace Shapez
 
 int main(int argc, char* argv[]) {
-  // using namespace Shapez;
-
   if (argc != 2) {
     std::cout << "Usage: solver dump.bin" << std::endl;
     return 1;
