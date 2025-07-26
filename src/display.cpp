b77c590147aeb41fdb16cb7d14054f115f65f3b7
FatcatX <garretsimpson@yahoo.com>
2025-07-26 13:42:44 -0700

    Update display.cpp
    cleanup

--- src/display.cpp
+++ src/display.cpp
@@ -14,36 +14,27 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  std::vector<Shape> halves;
-  std::vector<Shape> shapes;
-
   std::string filename = argv[1];
   ShapeSet shapeSet = ShapeSet::load(filename);
-  // std::copy_if(shapeSet.halves.begin(), shapeSet.halves.end(), std::back_inserter(halves),
-  //              [&](Shape shape) { return shape.layers() <= 4; });
-  // std::copy_if(shapeSet.shapes.begin(), shapeSet.shapes.end(), std::back_inserter(shapes),
-  //              [&](Shape shape) { return shape.layers() <= 4; });
-  halves = {shapeSet.halves.begin(), shapeSet.halves.end()};
-  shapes = {shapeSet.shapes.begin(), shapeSet.shapes.end()};
+
+  std::cout << "Halves: " << shapeSet.halves.size() << std::endl;
+  std::cout << "Shapes: " << shapeSet.shapes.size() << std::endl;
+  for (auto it : shapeSet.halves) {
+    std::cout << it.toString() << std::endl;
+  }
+  std::cout << std::endl;
+  for (auto it : shapeSet.shapes) {
+    std::cout << it.toString() << std::endl;
+  }
+  std::cout << std::endl;
   shapeSet.clear();
 
-  std::cout << "Halves: " << halves.size() << std::endl;
-  std::cout << "Shapes: " << shapes.size() << std::endl;
-  for (auto it : halves) {
-    std::cout << it.toString() << std::endl;
-  }
-  std::cout << std::endl;
-  for (auto it : shapes) {
-    std::cout << it.toString() << std::endl;
-  }
-  std::cout << std::endl;
-
-  SolutionSet solnSet = SolutionSet::load(filename);
-  std::sort(solnSet.solutions.begin(), solnSet.solutions.end());
-  for (auto it : solnSet.solutions) {
-    std::cout << it.toString() << std::endl;
-  }
-  solnSet.clear();
+  // SolutionSet solnSet = SolutionSet::load(filename);
+  // std::sort(solnSet.solutions.begin(), solnSet.solutions.end());
+  // for (auto it : solnSet.solutions) {
+  //   std::cout << it.toString() << std::endl;
+  // }
+  // solnSet.clear();
 
   return 0;
 }
